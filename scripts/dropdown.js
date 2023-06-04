38f642f873584ce98472acd11f0ebbeae8cd5347
VIRUS <sium15-13645@diu.edu.bd>
2023-06-04 21:45:11 +0600

    teacher page updated, dropdown.js and teacher.css newly added

---
+++ scripts/dropdown.js
@@ -0,0 +1,25 @@
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  dropdownContent.style.display =
+    dropdownContent.style.display === "block" ? "none" : "block";
+}
+
+function selectItem(item) {
+  var dropdownBtn = document.querySelector(".dropdown-btn");
+  dropdownBtn.textContent = item.textContent;
+  var dropdownContent = document.getElementById("dropdownContent");
+  dropdownContent.style.display = "none";
+}
+
+function toggleDropdownSection() {
+  var dropdownContent = document.getElementById("dropdownContentSection");
+  dropdownContent.style.display =
+    dropdownContent.style.display === "block" ? "none" : "block";
+}
+
+function selectItemSec(item) {
+  var dropdownBtn = document.querySelector(".dropdown-btn-section");
+  dropdownBtn.textContent = item.textContent;
+  var dropdownContent = document.getElementById("dropdownContentSection");
+  dropdownContent.style.display = "none";
+}
