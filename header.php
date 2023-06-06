87e42ba65245e7a0ddbef5429dd8d98ff9013686
Maruf <almaruf67@gmail.com>
2023-06-07 05:45:11 +0600

    almost

--- header.php
+++ header.php
@@ -5,29 +5,28 @@ session_start();
 $self_path = $_SERVER['PHP_SELF'];
 
 ?>
+
 <head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="css/style.css" />
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" />
   <link rel="stylesheet" href="css/teacher.css">
   <link rel="stylesheet" href="css/student.css" />
   <title><?php
-      if($self_path=="/project/teacherentry.php")
-       echo "Information";
-      else if($self_path=="/project/index.php")
-       echo "Home";
-      else if($self_path=="/project/teacher.php")
-       echo "Teacher";
-      else if($self_path=="/project/student.php")
-       echo "Student";
+          if ($self_path == "/project/teacherentry.php")
+            echo "Information";
+          else if ($self_path == "/project/index.php")
+            echo "Home";
+          else if ($self_path == "/project/teacher.php")
+            echo "Teacher";
+          else if ($self_path == "/project/student.php")
+            echo "Student";
 
-?></title>
+          ?></title>
 </head>
 
 <body class="body-color">
@@ -47,10 +46,10 @@ $self_path = $_SERVER['PHP_SELF'];
             </ul>
             <div class="profile-icon-width">
               <a onclick="profileDropDown();" href="#"><i class="fa-solid fa-user-tie"></i>
-              <p><?php echo $_SESSION['Name'] ?></p>
+                <p><?php echo $_SESSION['Name'] ?></p>
               </a>
             </div>
-            
+
             <button class="dropdown_button">
               <i onclick="showmenu(); changeIcon();" class="fa-solid fa-bars"></i>
             </button>
@@ -60,8 +59,8 @@ $self_path = $_SERVER['PHP_SELF'];
             <li><a href="#">About Us</a></li>
             <li><a href="#">Contact</a></li>
             <li><a class="profile-icon" onclick="profileDropDown();" href="#">
-              <p><?php echo $_SESSION['Name'] ?></p>
-            </a></li>
+                <p><?php echo $_SESSION['Name'] ?></p>
+              </a></li>
           </div>
         </div>
       </div>
@@ -70,8 +69,8 @@ $self_path = $_SERVER['PHP_SELF'];
       <div class="col-12">
         <div class="profile-drop">
           <div class="profiledropdownMenu" id="profileUL">
-            <li><a href="#">My Profile</a></li>
-            <li><a href="#">Notification</a></li>
+            <li><a href="#" data-bs-toggle="modal" data-bs-target="#proModal">My Profile</a></li>
+            <li><a href="#" data-bs-toggle="modal" data-bs-target="#notiModal">Notification</a></li>
             <li><a href="#">Settings</a></li>
             <li><a href="logout.php">Log Out</a></li>
           </div>
@@ -79,3 +78,55 @@ $self_path = $_SERVER['PHP_SELF'];
       </div>
     </div>
     <!-- navgigation bar close-->
+
+    <!-- Profile Modal -->
+    <div class="modal fade" id="proModal" tabindex="-1" aria-labelledby="proModal" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="proModal"><?php echo $_SESSION['Name'] ?></h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+
+          <div class="modal-body">
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Notification Modal -->
+    <div class="modal fade" id="notiModal" tabindex="-1" aria-labelledby="notiModal" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="notiModal"><?php echo $_SESSION['Name'] ?></h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <?php 
+              if($_SESSION['Name']=='teacher')
+              {
+                $tid=$_SESSION['ID'];
+                $sql="SELECT * FROM teacher WHERE id='$tid'";
+                $row = $result->fetch_assoc();
+
+              }
+          ?>
+          <div class="modal-body">
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+          </div>
+
+
+        </div>
+      </div>
+    </div>
