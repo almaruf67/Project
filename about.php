f72df7f32fdfe48d82559fe6fd0cdc95a1d31e53
VIRUS <sium15-13645@diu.edu.bd>
2023-06-07 00:12:05 +0600

    about us

---
+++ about.php
@@ -0,0 +1,99 @@
+<?php
+
+include_once("header.php");
+?>
+
+<div class="row">
+    <div class="col-12">
+        <div class="about-box">
+            <div class="container2">
+                <div class="grid-container">
+                    <div>
+                        <h2 class="reserver_text story_font"><br><br>Our Story</h2>
+                        <p class="about_text"><br><br>Mega was originally opened as Mega Seafood in 2005, housed in a historic space on Broad Street. <br>Four of our owners made it this far with nothing but scratch, and driven by their passion for incredible food and the hospitality industry. <br><br>The restaurant gave Siam a home to work from while still focusing on seafood, a culinary corner he has long excelled in, while also establishing his local restaurants– Mega Bistro and Main Street Meats– as pillars in the Chattanooga food scene.<br><br></p>
+                    </div>
+                    <div><img class="imagesize " src="images/res_2.jpg" alt=""></div>
+
+                </div>
+                <div class="member_overly">
+                    <h2 class="story_font2">Our Team</h2>
+                </div>
+
+            </div>
+            <div class="container2">
+                <div class="grid-container2">
+                    <div><img class="imagesize2" src="images/siam1.jpg" alt=""></div>
+                    <div>
+                        <h2 class="reserver_text self_font">Md. Siam Talukder</h2>
+                        <p class="info_font">
+                            ID: 201-15-13645<br>
+                            Section: D<br>
+                            Department of CSE
+                        </p>
+                        <img class="under_margin" src="images/common_logo.png" alt="">
+                        <div class="collapse">
+                            <ul class="navbar-nav">
+                                <a href="https://c.tenor.com/D-58xU7kmoEAAAAd/oiroke-konohagakure.gif" class="nav-link"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://c.tenor.com/D-58xU7kmoEAAAAd/oiroke-konohagakure.gif" class="nav-link"><i class="fab fa-twitter"></i></a>
+                                <a href="https://c.tenor.com/D-58xU7kmoEAAAAd/oiroke-konohagakure.gif" class="nav-link"><i class="fab fa-instagram"></i></a>
+                                <a href="https://github.com/virus1o1" class="nav-link"><i class="fab fa-github"></i></a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container2">
+                <div class="grid-container2">
+                    <div>
+                        <h2 class="reserver_text self_font name_margin">Afifatun Jannat</h2>
+                        <p class="info_font">
+                            ID: 201-15-13972<br>
+                            Section: D<br>
+                            Department of CSE
+                        </p>
+                        <img class="under_margin" src="images/common_logo.png" alt="">
+                        <div class="collapse">
+                            <ul class="navbar-nav">
+                                <a href="https://c.tenor.com/mQK9UQxFhj8AAAAC/love-live-nico-yazawa.gif" class="nav-link"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://c.tenor.com/mQK9UQxFhj8AAAAC/love-live-nico-yazawa.gif" class="nav-link"><i class="fab fa-twitter"></i></a>
+                                <a href="https://c.tenor.com/mQK9UQxFhj8AAAAC/love-live-nico-yazawa.gif" class="nav-link"><i class="fab fa-instagram"></i></a>
+                                <a href="https://github.com/Afifa-alo" class="nav-link"><i class="fab fa-github"></i></a>
+                            </ul>
+                        </div>
+                    </div>
+                    <div><img class="imagesize2" src="images/alo.jpg" alt=""></div>
+                </div>
+            </div>
+            <div class="container2">
+                <div class="grid-container2">
+                    <div><img class="imagesize2" src="images/maruf.jpg" alt=""></div>
+                    <div>
+                        <h2 class="reserver_text self_font2">Abdullah Al Maruf</h2>
+                        <p class="info_font2">
+                            ID: 201-15-13717<br>
+                            Section: D<br>
+                            Department of CSE
+                        </p>
+                        <img class="under_margin" src="images/common_logo.png" alt="">
+                        <div class="collapse">
+                            <ul class="navbar-nav">
+                                <a href="https://thatsthefinger.com/" class="nav-link"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://thatsthefinger.com/" class="nav-link"><i class="fab fa-twitter"></i></a>
+                                <a href="https://thatsthefinger.com/" class="nav-link"><i class="fab fa-instagram"></i></a>
+                                <a href="https://thatsthefinger.com/" class="nav-link"><i class="fab fa-github"></i></a>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<?php
+include_once("footer.php");
+?>
--- header.php
+++ header.php
@@ -9,6 +9,17 @@ $self_path = $_SERVER['PHP_SELF'];
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link 
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" 
+    rel="stylesheet">
+    <link 
+    href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Yeseva+One&display=swap" rel="stylesheet"> 
+    <link 
+    href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" 
+    rel="stylesheet"> 
+    <link 
+     href="https://fonts.googleapis.com/css2?family=Gentium+Book+Basic:wght@400;700&display=swap" 
+     rel="stylesheet">
   <link rel="stylesheet" href="css/style.css" />
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
@@ -17,6 +28,7 @@ $self_path = $_SERVER['PHP_SELF'];
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" />
   <link rel="stylesheet" href="css/teacher.css">
   <link rel="stylesheet" href="css/student.css" />
+  <link rel="stylesheet" href="css/about.css" />
   <title><?php
       if($self_path=="/project/teacherentry.php")
        echo "Information";
@@ -26,7 +38,8 @@ $self_path = $_SERVER['PHP_SELF'];
        echo "Teacher";
       else if($self_path=="/project/student.php")
        echo "Student";
-
+      else if($self_path=="/project/about.php")
+      echo "About Us";
 ?></title>
 </head>
 
