e81bbaad3bc545a132d8f7462c8ee4948f0a1440
Maruf <almaruf67@gmail.com>
2023-06-07 08:40:43 +0600

    Giving title for sudent page

--- student.php
+++ student.php
@@ -8,7 +8,8 @@ include_once("header.php");
       <div class="padding-first-row">
         <div class="col-6">
           <div class="search-chart-container">
-            <div class="search-chart-box px-5">
+            <div class="search-chart-box px-5 ">
+              <h5 class="titi">Search</h5>
               <div class="input-group">
                 <form action="" method="POST">
                   <div class="id-semester-display">
@@ -38,6 +39,7 @@ include_once("header.php");
         <div class="col-6">
           <div class="search-chart-container">
             <div class="search-chart-box">
+              <h5 class="titi">Result</h5>
               <?php
 
               if (isset($_POST['search'])) {
@@ -47,7 +49,8 @@ include_once("header.php");
                 $value = mysqli_query($mysqli, "SELECT * FROM result WHERE S_ID='$id'");
 
               ?>
-                <table border="0" class="table">
+
+                <table border="0" class="table mt-5">
                   <thead>
                     <tr>
                       <th scope="col">Course_Title</th>
@@ -110,6 +113,7 @@ include_once("header.php");
         <div class="col-6">
           <div class="gpa-container">
             <div class="gpa-box">
+              <h5 class="titi">Result Analysis</h5>
               <?php include("piechart.php"); ?>
               <div id="piechart" style="width: auto; height: 100%;" class="pt-5 px-5"></div>
             </div>
@@ -118,7 +122,8 @@ include_once("header.php");
         <div class="col-6">
           <div class="gpa-container">
             <div class="gpa-box px-5">
-              <table class="table">
+              <h5 class="titi">UGC Grading System</h5>
+              <table class="table mt-5">
                 <thead class="thead-dark">
                   <tr>
                     <th scope="col">Marks(%)</th>
@@ -192,7 +197,8 @@ include_once("header.php");
         <div class="barchart-container">
           <?php include("barchart.php"); ?>
           <div class="barchart-box">
-            <div id="top_x_div" style="height: 300px; padding:30px;" class="col-12"></div>
+            <h5 class="titi2">Report Generation</h5>
+            <div id="top_x_div" style="height: 300px; padding:30px;" class="col-12 mt-5"></div>
           </div>
         </div>
       </div>
@@ -201,97 +207,100 @@ include_once("header.php");
     <!-- Table chart -->
     <div class="row">
       <div class="col-12">
-        <div class="barchart-container barchart-box  px-2">
-          <?php
-          $id = $_SESSION['ID'];
-          require("config.php");
-          $value = mysqli_query($mysqli, "SELECT * FROM result WHERE S_ID='$id'");
+        <div class="barchart-container ">
+          <div class="barchart-box p-2">
+            <h5 class="titi2">Feedback & Recheck</h5>
+            <?php
+            $id = $_SESSION['ID'];
+            require("config.php");
+            $value = mysqli_query($mysqli, "SELECT * FROM result WHERE S_ID='$id'");
 
-          ?>
-          <table border="1" class="table">
-            <thead>
-              <tr>
-                <th scope="col">Course_Title</th>
-                <th scope="col">GPA</th>
-                <th scope="col">Feedback</th>
-                <th scope="col">Recheck Request</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              while ($item = $value->fetch_assoc()) {
-                $sum = $item['Quiz'] + $item['Mid'] + $item['Final'];
-                $val;
-                if ($sum > 79)
-                  $val = 4.00;
-                else if ($sum > 74)
-                  $val = 3.75;
-                else if ($sum > 69)
-                  $val = 3.50;
-                else if ($sum > 64)
-                  $val = 3.25;
-                else if ($sum > 59)
-                  $val = 3.00;
-                else if ($sum > 54)
-                  $val = 2.75;
-                else if ($sum > 49)
-                  $val = 2.50;
-                else if ($sum > 44)
-                  $val = 2.00;
-                else
-                  $val = 0.00;
-                $course = $item['Course_Title'];
-              ?>
+            ?>
+            <table border="1" class="table mt-5">
+              <thead>
                 <tr>
-                  <td><?php echo $item['Course_Title'] ?></td>
-                  <td><?php echo $val ?></td>
-                  <td><?php echo $item['Feedback'] ?></td>
-                  <td><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#checkModal<?php echo $sid ?>">
-                      <i class="fa-solid fa-repeat" style="font-size: 20px; margin-top: -5px; color: #212529;"></i>
-                    </button></td>
+                  <th scope="col">Course_Title</th>
+                  <th scope="col">GPA</th>
+                  <th scope="col">Feedback</th>
+                  <th scope="col">Recheck Request</th>
                 </tr>
+              </thead>
+              <tbody>
+                <?php
+                while ($item = $value->fetch_assoc()) {
+                  $sum = $item['Quiz'] + $item['Mid'] + $item['Final'];
+                  $val;
+                  if ($sum > 79)
+                    $val = 4.00;
+                  else if ($sum > 74)
+                    $val = 3.75;
+                  else if ($sum > 69)
+                    $val = 3.50;
+                  else if ($sum > 64)
+                    $val = 3.25;
+                  else if ($sum > 59)
+                    $val = 3.00;
+                  else if ($sum > 54)
+                    $val = 2.75;
+                  else if ($sum > 49)
+                    $val = 2.50;
+                  else if ($sum > 44)
+                    $val = 2.00;
+                  else
+                    $val = 0.00;
+                  $course = $item['Course_Title'];
+                ?>
+                  <tr>
+                    <td><?php echo $item['Course_Title'] ?></td>
+                    <td><?php echo $val ?></td>
+                    <td><?php echo $item['Feedback'] ?></td>
+                    <td><button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#checkModal<?php echo $sid ?>">
+                        <i class="fa-solid fa-repeat" style="font-size: 20px; margin-top: -5px; color: #212529;"></i>
+                      </button></td>
+                  </tr>
 
 
-                <!-- Modal Recheck-->
-                <div class="modal fade" id="checkModal<?php echo $sid ?>" tabindex="-1" aria-labelledby="checkModal" aria-hidden="true">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="checkModal">Recheck Application for <?php echo $sid ?></h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  <!-- Modal Recheck-->
+                  <div class="modal fade" id="checkModal<?php echo $sid ?>" tabindex="-1" aria-labelledby="checkModal" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="checkModal">Recheck Application for <?php echo $sid ?></h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form method="POST" action="markupdate.php">
+                          <div class="modal-body">
+
+
+                            <input type="hidden" value="<?php echo $sid ?>" name="sid">
+                            <input type="hidden" value="<?php echo $course ?>" name="c_id">
+
+                            <div class="mb-3">
+                              <label for="subject<?php echo $sid ?>" class="form-label">Subject</label>
+                              <input name="subject" id="subject<?php echo $sid ?>" class="form-control">
+
+                            </div>
+                            <div class="mb-3">
+                              <label for="des<?php echo $sid ?>" class="form-label">Description</label>
+                              <textarea name="des" id="des<?php echo $sid ?>" class="form-control" cols="20" rows="5"></textarea>
+
+                            </div>
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary" name="recheck">Save changes</button>
+                          </div>
+                        </form>
+
                       </div>
-                      <form method="POST" action="markupdate.php">
-                        <div class="modal-body">
-
-
-                          <input type="hidden" value="<?php echo $sid ?>" name="sid">
-                          <input type="hidden" value="<?php echo $course ?>" name="c_id">
-
-                          <div class="mb-3">
-                            <label for="subject<?php echo $sid ?>" class="form-label">Subject</label>
-                            <input name="subject" id="subject<?php echo $sid ?>" class="form-control">
-
-                          </div>
-                          <div class="mb-3">
-                            <label for="des<?php echo $sid ?>" class="form-label">Description</label>
-                            <textarea name="des" id="des<?php echo $sid ?>" class="form-control" cols="20" rows="5"></textarea>
-
-                          </div>
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                          <button type="submit" class="btn btn-primary" name="recheck">Save changes</button>
-                        </div>
-                      </form>
-
                     </div>
                   </div>
-                </div>
 
-              <?php }
-              ?>
-            <tbody>
-          </table>
+                <?php }
+                ?>
+              <tbody>
+            </table>
+          </div>
         </div>
       </div>
 
